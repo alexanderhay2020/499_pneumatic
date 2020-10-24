a74c4e4e2ee538b2405d601115c5511f8b72603e
alex hay <alexanderhay2020@u.northwestern.edu>
2020-10-24 01:00:54 -0500

    PIC reads toggle switch input

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -37,13 +37,20 @@ int main() {
     SPI1_init();
     LCD_init();
     LCD_clearScreen(ILI9341_BLACK);
+    ui();
 
     // set pins
     TRISAbits.TRISA4 = 0;           // sets A4 as output
     LATAbits.LATA4 = 0;             // sets A4 to low
-
-    // variables
+    TRISBbits.TRISB11 = 1;          // RB11 as input
+    TRISBbits.TRISB12 = 1;          // RB12 as input
+    TRISBbits.TRISB13 = 0;          // RB13 as output
+    LATBbits.LATB13 = 1;            // 0=0v, 1=3.2v
     
+    // variables
+    char temp_msg[20];
+    int index = 0;
+
     while (1) {
         // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
         // remember the core timer runs at half the sysclk
@@ -60,7 +67,21 @@ int main() {
             LCD_drawPixel(0,0,ILI9341_BLACK); // flashes single LED on screen
         }
 
-        ui();
+        
+        index = 0;
+            sprintf(temp_msg, "RB11, Pin 22:   %d",PORTBbits.RB11);
+            while(temp_msg[index]) {
+                print_char(28 + 5*index, 56, temp_msg[index]);
+                index++;
+            }
+//        }
+//        else {//if(PORTBbits.RB11 == 1){
+            index = 0;
+            sprintf(temp_msg, "RB12, Pin 23:   %d",PORTBbits.RB12);
+            while(temp_msg[index]) {
+                print_char(28 + 5*index, 72, temp_msg[index]);
+                index++;
+            }
         
         LATAbits.LATA4 = !LATAbits.LATA4;
 
